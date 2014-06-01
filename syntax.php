6fa44feeb6a3f24b9f11f38c3cf9353dcb34f933
Jason Xun Xu <dev@jasonxu.net>
2014-06-01 13:03:38 -0700

    * Better comment handling respecting quoted strings, based on [this](http://stackoverflow.com/a/241506/707516).

--- syntax.php
+++ syntax.php
@@ -60,10 +60,15 @@ class syntax_plugin_c3chart extends DokuWiki_Syntax_Plugin {
             $opts[strtolower($m[1])] = $m[2];
         }
 
-        $c3data = preg_replace('#//.*?$#m', '', $c3data); // comment: // foo
+        $c3data = preg_replace_callback(
+            '#//.*?$|/\*.*?\*/|\'(?:\\.|[^\\\'])*\'|"(?:\\.|[^\\"])*"#ms',
+            function($matches){
+                $m = $matches[0];
+                return substr($m, 0, 1)==='/' ? ' ' : $m;
+            }, $c3data
+        ); // remove comments (respecting quoted strings)
         $c3data = explode("\n", $c3data);
         $c3data = implode("", array_map(trim, $c3data));
-        $c3data = preg_replace('#/\*.*?\*/#', '', $c3data); // comment: /* bar */
         if($c3data[0]=='{') $c3data = substr($c3data, 1, -1);
         $chartid = uniqid('__c3chart_');
         $c3data = base64_encode('{"bindto": "#'.$chartid.'",'.$c3data.'}');
